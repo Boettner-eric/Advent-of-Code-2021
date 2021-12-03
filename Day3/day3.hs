6f7300ea9cefd35dcc4901f902187bc7dfb5b431
eric-boettner <boettner.eric@gmail.com>
2021-12-03 00:03:43 -0800

    Day 3 part one

--- Day3/day3.hs
+++ Day3/day3.hs
@@ -1,12 +1,34 @@
+import Data.Char
+import Data.Bool(bool)
+
 main = do
     txt <- readFile "day3.txt"
-    let binary = lines txt
-    splitBinary binary
+    let binary = splitBinary (lines txt)
+    let epsilon = convertSum True <$> binary
+    let gamma = convertSum False <$> binary
+    print (bintodec epsilon * bintodec gamma)
 
-splitBinary :: [String] -> [[Ints]]
-splitBinary [] -> [[]]
-splitBinary (x:xs) -> zipWith (+) [binString x] splitBinary xs
+-- take sum of digits and convert to bool
+-- True -> GCB, False -> LCB
+convertSum :: Bool -> Int -> Bool
+convertSum True x = x > 500
+convertSum _ x = x < 500
 
+-- part 2
+-- take convertSum and if True filter out starting in 0 else 1
+-- if even then pick the one
+
+-- convert string of digits to array of ints
 binString :: String -> [Int]
-binString [] -> []
-binString (x:xs) -> [read x] ++ binString xs
+binString [] = []
+binString (x:xs) = [ord x - 48] ++ (binString xs)
+
+-- add each power or digit together
+splitBinary :: [String] -> [Int]
+splitBinary [] = take 12 (repeat 0) -- fill empty array
+splitBinary (x:xs) = zipWith (+) (binString x) (splitBinary xs)
+
+-- had to look this one up due to lack of knowledge
+--https://stackoverflow.com/questions/56663207/binary-to-decimal-in-haskell-without-using-recursion-or-list-comprehensions?rq=1
+bintodec :: (Foldable f, Integral i) => f Bool -> i
+bintodec = foldl (\a -> (+) (2*a) . bool 0 1) 0
