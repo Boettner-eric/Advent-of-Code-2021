d41fdefa9b31d804770e15b097580c3344226716
eric-boettner <boettner.eric@gmail.com>
2021-12-06 16:27:30 -0800

    Replaced bad function with Map's default fn mapKeys

--- Day6/day6.hs
+++ Day6/day6.hs
@@ -4,6 +4,7 @@ main = do
     txt <- readFile "day6.txt"
     let dataset = map read (split (==',') txt) :: [Int]
     let fish = Map.fromListWith (+) (map (\i -> (i,1)) dataset) :: Fish
+    print (sum (Map.elems (runDays 80 fish)))
     print (sum (Map.elems (runDays 256 fish)))
 
 type Fish = Map.Map Int Int
@@ -12,8 +13,7 @@ runDays :: Int -> Fish -> Fish
 runDays 0 fish = fish
 runDays i fish = runDays (i-1) ret
     where
-        adjust (key, x) = if key == 0 then (8, x) else (key-1,x)
-        newfish = Map.fromList (map adjust (Map.toAscList fish))
+        newfish = Map.mapKeys (\n -> if n == 0 then 8 else n-1) fish
         spawn = Map.findWithDefault 0 0 fish -- count number of zeros
         ret = Map.insertWith (+) 6 spawn newfish -- add new fish to pool
 
