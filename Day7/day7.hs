0cefa7d46cca3a8ebf0a0c341271d1b794c2fdf5
eric-boettner <boettner.eric@gmail.com>
2021-12-07 17:10:10 -0800

    Fixed bad comment and added details

--- Day7/day7.hs
+++ Day7/day7.hs
@@ -5,11 +5,12 @@ main = do
     txt <- readFile "day7.txt"
     let dataset = map read (split (==',') txt) :: [Int]
     let items = map (\n -> (countSteps n dataset)) [(minimum dataset) .. (maximum dataset)] -- test every number between min/max val
-    print (minimumBy (comparing snd) items) -- find smallest value pair (index, fuel cost)
+    print (minimumBy (comparing snd) items) -- find smallest fuel cost from pair (index, fuel cost)
 
+-- I included the index with the fuel cost in case it was needed (it wasn't)
 countSteps :: Int -> [Int] -> (Int, Int)
 countSteps i list = (i, sum (map (\n -> div ( (abs (n-i)+1) * ( (abs (n-i))) ) 2) list)) -- cost of making that step for every value
--- (n (n-1) / 2) calculates sum of all values less than n - abs (n-i) if the number of steps and we add one to include final step
+-- 1 + ... + n = div ( (abs (n-i)+1) * ( (abs (n-i))) ) 2
 
 split :: (Char -> Bool) -> String -> [String]
 split p s =  case dropWhile p s of
