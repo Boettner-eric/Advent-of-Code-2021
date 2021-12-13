29b668337e9fbcc1d4e217d4eb0a8ad57a064c68
eric-boettner <boettner.eric@gmail.com>
2021-12-13 11:51:21 -0800

    Finally fixed solution for Part2 - This is still super innefficient - I needed to use a better structure (filtering lowercase each time is very bad)

--- Day12/day12.hs
+++ Day12/day12.hs
@@ -1,31 +1,28 @@
 import Data.Char
 import Data.Set (toList, fromList)
-import Data.List
+import Data.List (nub, isInfixOf)
 
 main = do
-    txt <- readFile "sample.txt"
+    txt <- readFile "day12.txt"
     let input = map (splitl (=='-')) (lines txt)
-    let y = findPath input input "start" "start" False
-    print (length y)
-    let x = findPath input input "start" "start" True
-    print (length x)
-    let results = toList (fromList (x))
-    print (length results)
+    let part1 = findPath input input "start" ["start"] 1
+    print (length part1)
+    let part2 = findPath input input "start" ["start"] 2
+    print (length part2)
 
-findPath :: [[String]] -> [[String]] -> String -> String -> Bool -> [String]
+findPath :: [[String]] -> [[String]] -> String -> [String] -> Int -> [[String]]
 findPath _ [] _ current _ = []
-findPath graph (x:xs) start current twice
+findPath graph ([x,y]:xs) start current allowed
     | start == "end" = [current]
-    | start /= "start" && "start" `elem` x = next
-    | ((head x) ++ ",") `isInfixOf` current && allLower (head x) && twice = next
-    | ((last x) ++ ",") `isInfixOf` current && allLower (last x) && twice = next
-    | head x == start = findPath graph graph (last x) (current ++',': last x) (i || j || twice) ++ next
-    | last x == start = findPath graph graph (head x) (current ++',': head x) (i || j || twice) ++ next
+    | start /= "start" && "start" `elem` [x,y] = next
+    | twice = next
+    | x == start = findPath graph graph y (current ++ [y]) allowed ++ next
+    | y == start = findPath graph graph x (current ++ [x]) allowed ++ next
     | otherwise = next
         where
-            i = ((head x) ++ ",") `isInfixOf` current && allLower (head x)
-            j = ((last x) ++ ",") `isInfixOf` current && allLower (last x)
-            next = findPath graph xs start current (i || j || twice)
+            smallCaves = filter allLower current
+            twice = length smallCaves > length (nub smallCaves) + allowed -1
+            next = findPath graph xs start current allowed
 
 allLower i = all (==True) (map isLower i)
 
