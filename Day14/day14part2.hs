420b6e7b92ba5b62bf4e163378b3de623635fcfd
eric-boettner <boettner.eric@gmail.com>
2021-12-14 16:48:46 -0800

    The bug is very sad - Imported data with fromList instead of fromListWith (+) so some data was ommited - I'm very tired

--- Day14/day14part2.hs
+++ Day14/day14part2.hs
@@ -4,34 +4,28 @@ import Data.List
 
 import qualified Data.Map as Map
 import Data.Map (Map)
--- idea for part2
-{-
-    Make a map of each pair (Char,Char) -> Int
-    for each step:
-        if a pair is destroyed decrement its counter and add to counters for
-            (a,b) (a,b,c) -> (a,c)+1, (c,b)+1, (a,b)-1
-        if a new pair is found add a mapping for it
 
--}
 type Rules = [(Char, Char, Char)]
 type Pairs = Map (Char, Char) Int
 type CountChar = Map Char Int
 
 main = do
-    txt <- readFile "sample.txt"
+    txt <- readFile "day14.txt"
     let Right((template, rules)) = parse parser "" txt
-    let list = Map.fromList (splitWord template)
-    print list
+    let list = Map.fromListWith (+) (splitWord template)
     let freqChar = Map.fromListWith (+) (map (\n -> (n,1)) template)
-    let finalFreq = step 40 (list, freqChar) rules
-    let part2 = sortBy (comparing snd) (Map.toList finalFreq)
-    print finalFreq
+    let part1Base = step 10 (list, freqChar) rules
+    let part1 = sortBy (comparing snd) (Map.toList part1Base)
+    print (snd(last part1) - snd(head part1) )
+    let part2Base = step 40 (list, freqChar) rules
+    let part2 = sortBy (comparing snd) (Map.toList part2Base)
     print part2
     print (snd(last part2) - snd(head part2) )
 
+-- correct
 splitWord :: String -> [((Char, Char), Int)]
 splitWord [] = []
-splitWord (i:[]) = []
+splitWord [i] = []
 splitWord (i:j:xs) = ((i, j), 1) : splitWord (j:xs)
 
 step :: Int -> (Pairs, CountChar) -> Rules -> CountChar
@@ -43,8 +37,9 @@ adjustVals pairMap freqChar [] = (pairMap, freqChar)
 adjustVals pairMap freqChar ((i,j,c):xs) = (Map.unionWith (+) thisPair nextPair, Map.insertWith (+) c matches nextCount)
     where
         matches = Map.findWithDefault 0 (i,j) pairMap
-        thisPair = Map.fromList [((i,c), matches), ((c,j), matches)]
-        (nextPair, nextCount) = adjustVals (Map.delete (i,j) pairMap) freqChar xs
+        newMap = if matches > 0 then Map.delete (i,j) pairMap else pairMap
+        thisPair = if matches > 0 then Map.fromList [((i,c), matches), ((c,j), matches)] else Map.empty
+        (nextPair, nextCount) = adjustVals newMap freqChar xs
 
 parser :: Parsec String () (String, Rules)
 parser = do
